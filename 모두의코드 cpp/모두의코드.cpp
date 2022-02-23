4e70db0a4d92844901392bbfd7a26c1906008212
Younjaehyung <104jake1217@gmail.com>
2022-02-24 06:54:42 +0900

    모두의코드 #1 참조자(reference)-배열

--- 모두의코드 cpp/모두의코드.cpp
+++ 모두의코드 cpp/모두의코드.cpp
@@ -5,13 +5,16 @@ using namespace std;
 참조자(reference)
 배열들의 레퍼런스o
 래퍼런스 배열X
+
 */
 
 
 int main() {
 	int arr[3] = { 1, 2, 3 };
 	int(&ref)[3] = arr;
-
+	/*
+    int &ref[3]={1,2,3}; >>래퍼런스 배열 x
+	*/
 	ref[0] = 2;
 	ref[1] = 3;
 	ref[2] = 1;
