997f22136762ec184cef692ca4d39a0aec2d706b
Younjaehyung <104jake1217@gmail.com>
2022-02-25 05:03:34 +0900

    모두의코드# 2동적할당 ( new / delete)

---
+++ 모두의코드 cpp/모두의코드2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+/*
+모두의코드# 2
+동적할당 ( new / delete)
+
+동적할당 배열
+*/
+int main() {
+    int arr_size;
+    std::cout << "array size : ";
+    std::cin >> arr_size;
+    int* list = new int[arr_size];
+    for (int i = 0; i < arr_size; i++) {
+        std::cin >> list[i];
+    }
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << i << "th element of list : " << list[i] << std::endl;
+    }
+    delete[] list;
+
+}
