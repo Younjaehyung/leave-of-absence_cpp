98d43fd3b6cc8f9dae757a3c2cf4b0025177a8aa
Younjaehyung <104jake1217@gmail.com>
2022-02-24 23:34:21 +0900

    외부변수 참조자

--- 모두의코드 cpp/모두의코드.cpp
+++ 모두의코드 cpp/모두의코드.cpp
@@ -4,20 +4,24 @@ using namespace std;
 모두의코드# 1 
 참조자(reference)
 Dangling reference
-지역변수 참조자
+외부변수 참조자
 
 -----
-함수 리턴값의 유형이 레퍼런스이지만 리턴값은 지역내에서만 존재 하기때문에
-리턴할시 int b= function(); 는 a를 참조 못하게 된다
-허나 비쥬얼스튜디오에서는 컴파일러가 알아서 하게 됨.
+위와 같이 인자로 받은 레퍼런스를 그대로 리턴 하고 있습니다.
+
+function(b) 를 실행한 시점에서 a 는 main 의 b 를 참조하고 있게 됩니다. 
+따라서 function 이 리턴한 참조자는 아직 살아있는 변수인 b 를 계속 참조 합니다.
+
+int c = function(b);
+결국 위 문장은 그냥 c 에 현재의 b 의 값인 5 를 대입하는 것과 동일한 문장이 됩니다.
 */
-int& function() {
-	int a = 2;
+int& function(int& a) {
+	a = 5;
 	return a;
 }
 
 int main() {
-	int b = function();
-	b = 3;
+	int b = 2;
+	int c = function(b);
 	return 0;
 }
